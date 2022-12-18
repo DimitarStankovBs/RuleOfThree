d25e20b7f9d2d5c1ac91c4d751acfb3b95cd160c
DimitarStankov <116435607+DimitarStankov@users.noreply.github.com>
2022-12-18 09:53:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RuleOfThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer:");
+            int num = int.Parse(Console.ReadLine());
+            int I = 0;
+
+            while (num==0)
+            {
+                if (num%2==0)
+                {
+                    num=num/2;
+                }
+                else 
+                {
+                    num = num * 3 + 1;
+                }I++;
+            }
+            Console.WriteLine(I+ num);
+        }
+    }
+}
